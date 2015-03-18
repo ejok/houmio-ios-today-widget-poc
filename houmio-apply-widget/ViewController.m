2acbb002beffd0696ce72e4e0280fa4ac2201934
Eeli Jokinen <eeli.jokinen@gmail.com>
2015-03-18 23:18:23 +0200

    add data loading based on sitekey

--- houmio-apply-widget/ViewController.m
+++ houmio-apply-widget/ViewController.m
@@ -11,20 +11,59 @@
 @interface ViewController ()
 
 @property (weak, nonatomic) IBOutlet UITextField *siteKey;
+@property NSUserDefaults *sharedDefaults;
 @end
 
 @implementation ViewController
 
 - (IBAction)getScenesButtonPressed:(id)sender {
-    NSUserDefaults *sharedDefaults = [[NSUserDefaults alloc] initWithSuiteName:@"group.fi.nipe.HoumioApplyWidget"];
-    [sharedDefaults setObject:self.siteKey.text forKey:@"siteKey"];
-    [sharedDefaults synchronize];
+    [self.sharedDefaults setObject:self.siteKey.text forKey:@"siteKey"];
+    [self.sharedDefaults synchronize];
+    [self loadScenes:self];
+}
+
+- (NSURLConnection *)loadScenes: (NSObject *) delegate {
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:[@"https://houmi.herokuapp.com/api/site/" stringByAppendingString:self.siteKey.text]]];
+    [request setHTTPMethod:@"GET"];
+    return [[NSURLConnection alloc] initWithRequest:request delegate:delegate];
+}
+
+-  (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *) data {
+    NSError *error;
+    NSDictionary *site = [NSJSONSerialization
+                              JSONObjectWithData:data
+                              options:kNilOptions
+                              error:&error];
+
+    NSMutableDictionary *scenes = [site valueForKey:@"scenes"];
+    [self.sharedDefaults setObject:scenes forKey:@"scenes"];
+}
+
+- (void)showLoadingError {
+    [[[UIAlertView alloc] initWithTitle:@"Error"
+                                message:@"Unable to fetch scenes. Check the site key!"
+                               delegate:nil
+                      cancelButtonTitle:NSLocalizedString(@"OK", @"")
+                      otherButtonTitles:nil] show];
+}
+
+- (void)connection:(NSURLConnection*)connection didReceiveResponse:(NSURLResponse*)response {
+    NSHTTPURLResponse* httpResponse = (NSHTTPURLResponse*)response;
+
+    if ([httpResponse statusCode] != 200) {
+        [self showLoadingError];
+    }
+}
+
+- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error {
+    [self showLoadingError];
 }
 
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.sharedDefaults = [[NSUserDefaults alloc] initWithSuiteName:@"group.fi.nipe.HoumioApplyWidget"];
+    self.siteKey.text = [self.sharedDefaults objectForKey:@"siteKey"];
 }
 
 - (void)didReceiveMemoryWarning {
